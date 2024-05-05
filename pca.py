d2110fecb3669cf013b1b52309e9bc3922f334a5
karol <karol.kruszynski0@gmail.com>
2024-05-05 18:16:56 +0200

    added: - Species from dataset - 3d scatterplot

--- pca.py
+++ pca.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 # Load Data
 iris = datasets.load_iris()
+species = iris.target # Species
 
 # Make PCA Analysis
 covar_matrix = PCA(n_components=4)  # Check for the 4 feature sepal len,width petal len,width
@@ -27,3 +28,19 @@ plt.show()
 
 # Dimensional Reduction
 x_reduced = PCA(n_components=3).fit_transform(iris.data)
+
+# Building 3D Scatterplot for new values
+from mpl_toolkits.mplot3d import Axes3D
+
+# SCATTERPLOT 3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_title("Iris Dataset by PCA", size=14)
+ax.scatter(x_reduced[:,0], x_reduced[:,1], x_reduced[:,2], c=species)
+ax.set_xlabel("First eigenvector")
+ax.set_ylabel("Second eigenvector")
+ax.set_zlabel("Third eigenvector")
+ax.xaxis.set_ticklabels(())
+ax.yaxis.set_ticklabels(())
+ax.zaxis.set_ticklabels(())
+plt.show()
