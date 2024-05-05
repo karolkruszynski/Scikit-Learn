6111eff764072e2232fe71eddb952275a80e6f54
karol <karol.kruszynski0@gmail.com>
2024-05-05 18:01:45 +0200

    added comments Dimensional Reduction

--- pca.py
+++ pca.py
@@ -3,14 +3,17 @@ import numpy as np
 from sklearn import datasets
 import matplotlib.pyplot as plt
 
+# Load Data
 iris = datasets.load_iris()
 
+# Make PCA Analysis
 covar_matrix = PCA(n_components=4)  # Check for the 4 feature sepal len,width petal len,width
 covar_matrix.fit(iris.data)
 variance = covar_matrix.explained_variance_ratio_
 var = np.cumsum(np.round(variance, decimals=3)*100)
 print(var)
 
+# Plot the score of PCA
 plt.ylabel("% Variance explained")
 plt.xlabel("# of Features")
 plt.title("PCA Analysis")
@@ -19,3 +22,8 @@ plt.xticks([0,1,2,3], [1,2,3,4])
 plt.axvline(2, linestyle="--", c="#bbbbbb")
 plt.plot(var)
 plt.show()
+
+# 3 features is enough to build good model
+
+# Dimensional Reduction
+x_reduced = PCA(n_components=3).fit_transform(iris.data)
