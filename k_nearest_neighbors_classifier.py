7bdd96e3b7d3b570baf2752f738e609ce7e836bc
karol <karol.kruszynski0@gmail.com>
2024-05-11 18:15:09 +0200

    Apply KNN

--- k_nearest_neighbors_classifier.py
+++ k_nearest_neighbors_classifier.py
@@ -3,6 +3,7 @@
 import numpy as np
 from sklearn import datasets
 
+# Preparing Data
 np.random.seed(0)
 iris = datasets.load_iris()
 x = iris.data
@@ -12,3 +13,16 @@ x_train = x[i[:-10]]
 y_train = y[i[:-10]]
 x_test = x[i[-10:]]
 y_test = y[i[-10:]]
+
+#Apply KNN
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier()
+knn.fit(x_train,y_train)
+KNeighborsClassifier(algorithm='auto',
+                     leaf_size = 30,
+                     metric = 'minkowski',
+                     metric_params=None,
+                     n_neighbors=5,
+                     p=2,
+                     weights='uniform')
+
